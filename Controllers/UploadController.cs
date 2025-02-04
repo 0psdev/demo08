b85e10b6f5084e10fdc391e870565a091aeffd88
OpsDev <ops2dev@hotmail.com>
2025-02-04 20:44:28 +0700

    Initial project

---
+++ Controllers/UploadController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo08.Controllers
+{
+    public class UploadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
